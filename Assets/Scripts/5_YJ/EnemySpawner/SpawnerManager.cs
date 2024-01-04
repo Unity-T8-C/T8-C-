b42514ba91911d811831603e6fc79f746637dcca
joduhee <engml9252@gmail.com>
2024-01-04 16:18:24 +0900

    에너미 사이드에서 출현
    스포너 생성을 4개 늘려서 9개생성

--- Assets/Scripts/5_YJ/EnemySpawner/SpawnerManager.cs
+++ Assets/Scripts/5_YJ/EnemySpawner/SpawnerManager.cs
@@ -25,9 +25,26 @@ public class SpawnerManager : MonoBehaviour
     void SpawnEnemy()
     {
         int ranEnemy = Random.Range(0, 3);
-        int ranPoint = Random.Range(0, 5);
-        Instantiate(enemyObjs[ranEnemy],
+        int ranPoint = Random.Range(0, 9);
+        GameObject enemy= Instantiate(enemyObjs[ranEnemy],
             spawnPoints[ranPoint].position,
                 spawnPoints[ranPoint].rotation);
+
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        EnemyMovement enemyLogic = enemy.GetComponent<EnemyMovement>();
+        
+
+        if(ranPoint ==5 ||ranPoint ==6)
+        {
+            rigid.velocity = new Vector2(enemyLogic.speed * (-1), 1);
+        }
+        else if(ranPoint ==7||ranPoint==8)
+        {
+            rigid.velocity = new Vector2(enemyLogic.speed, 1);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(0,enemyLogic.speed*(-1));
+        }
     }
 }
