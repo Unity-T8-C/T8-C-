03b675065af6e3c3dfa1daca60f00273a928c519
민현영 <alsgsudud077@naver.com>
2024-01-05 20:10:37 +0900

    StoryScene
    [추가]
    스토리 슬라이드 애니메니션 동작
    마지막 장면에서 게임 시작 씬으로 넘어갑니다.

--- Assets/Scripts/1/StorySlidShow.cs
+++ Assets/Scripts/1/StorySlidShow.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class StorySlideShow : MonoBehaviour
@@ -7,6 +8,7 @@ public class StorySlideShow : MonoBehaviour
     public Image storyImage; // 스토리 이미지를 표시할 UI 이미지
     public Sprite[] storySprites; // 스토리 이미지 배열
     public float changeInterval = 5f; // 이미지 변경 간격 (초)
+    public string StartSceneName = "StartScene"; // 게임 시작 씬 이름
 
     private int currentImageIndex = 0;
 
@@ -20,6 +22,17 @@ public class StorySlideShow : MonoBehaviour
         while (true)
         {
             storyImage.sprite = storySprites[currentImageIndex];
+
+            // 마지막 이미지에 도달했는지 확인
+            if (currentImageIndex == storySprites.Length - 1)
+            {
+                yield return new WaitForSeconds(changeInterval);
+
+                // 게임 시작 씬으로 전환
+                SceneManager.LoadScene(StartSceneName);
+                break;
+            }
+
             currentImageIndex = (currentImageIndex + 1) % storySprites.Length;
             yield return new WaitForSeconds(changeInterval);
         }
