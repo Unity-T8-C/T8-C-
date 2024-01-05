53b3b66ab52c04da41f7fa66b4fb201178ba561c
colarrot <gunho7935@gmail.com>
2024-01-05 20:06:38 +0900

    BombFixed

---
+++ Assets/Scripts/2/Items/Item.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType { Boom }
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private ItemType itemType;
+    private Movement2D movement2D;
+
+    private void Awake()
+    {
+        movement2D = GetComponent<Movement2D>();
+
+        float x = Random.Range(-1.0f, 1.0f);
+        float y = Random.Range(-1.0f, 1.0f);
+
+        movement2D.MoveTo(new Vector3(x, y, 0));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            
+            Use(collision.gameObject);
+            
+            Destroy(gameObject);
+        }
+    }
+
+    public void Use(GameObject player)
+    {
+        switch (itemType)
+        {
+            case ItemType.Boom:
+                player.GetComponent<Explosion>().BoomCount++;
+                break;
+        }
+    }
+}
