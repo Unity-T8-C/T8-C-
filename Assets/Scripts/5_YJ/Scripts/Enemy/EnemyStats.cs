535d5a27fb8227aa9ae1eab3c76ae2f211591852
민현영 <alsgsudud077@naver.com>
2024-01-09 11:42:14 +0900

    Victory Scene

--- Assets/Scripts/5_YJ/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/5_YJ/Scripts/Enemy/EnemyStats.cs
@@ -12,7 +12,7 @@ public class EnemyStats
 {
     public StatsChangeType statsChangeType;
 
-    [Range(1, 1000)] public int maxHp;
+    [Range(1, 5000)] public int maxHp;
     [Range(1.0f, 20.0f)] public float moveSpeed;
 
     public int attackDamage;
